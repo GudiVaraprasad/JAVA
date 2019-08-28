ad29e311bef12ba2287bf6b87ad6d715d506f846
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2019-08-28 22:01:37 +0530

    Add files via upload

---
+++ Skeleton of JAVA.Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+       Scanner in=new Scanner(System.in);
+       
+       //Code 
+    
+      System.out.println();  
+    }
+
+}
