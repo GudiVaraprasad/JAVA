d7e3eb1637a5febc01f43240cab8e63ce66fa04d
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-08-15 18:44:01 +0530

    Create nestedClass.java

---
+++ OOPS/nestedClass.java
@@ -0,0 +1,31 @@
+
+
+import java.util.*;
+class C{
+    int i;
+    static int k;
+    void Method()
+    {
+        B b=new B();
+        b.j=10;
+        System.out.println(b.j);
+}
+class B{
+    int j;
+    void Method(){
+        i=10;
+        k=15;
+        j=12;
+        System.out.println(j);
+    }
+}
+}
+public class nestedClass
+{
+    public static void main(String[] args) {
+        C c=new C();
+        C.B b=c.new B();  // nested class syntax
+        b.Method();  // output is 12
+        c.Method();  // output is 10 
+    }
+}
